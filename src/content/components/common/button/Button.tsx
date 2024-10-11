83995a507af7dbec4e425b35cc33240f148c3ceb
leejeongho <jh9628771@gmail.com>
2024-10-11 21:46:58 +0900

    refact: 버튼 컴포넌트 수정

---
+++ src/content/components/common/button/Button.tsx
@@ -0,0 +1,21 @@
+import { ButtonHTMLAttributes, forwardRef, PropsWithChildren } from "react";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export const Button = forwardRef<
+  HTMLButtonElement,
+  PropsWithChildren<ButtonProps>
+>((props, ref) => {
+  const { disabled, children, ...restProps } = props;
+
+  return (
+    <button
+      ref={ref}
+      className="flex items-center justify-center text-[1.4rem] font-normal rounded-[0.8rem] px-5 py-4 transition-colors bg-mainBlue text-white cursor-pointer disabled:pointer-events-none disabled:bg-gray disabled:text-black "
+      disabled={disabled}
+      {...restProps}
+    >
+      {children}
+    </button>
+  );
+});
