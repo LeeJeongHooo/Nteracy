63a1be50c0f854328c93e6525d8885aa25f1876f
leejeongho <jh9628771@gmail.com>
2024-10-18 21:21:16 +0900

    fix: container export 수정

--- src/content/components/common/container/Container.tsx
+++ src/content/components/common/container/Container.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import { ReactNode } from "react";
 
-interface ContainerProps {
+export interface ContainerProps {
   children: ReactNode;
 }
 
-const Container = ({ children }: ContainerProps) => {
+export const Container = ({ children }: ContainerProps) => {
   return (
     <div className="mt-2 mx-auto bg-slate-100 rounded-2xl overflow-hidden mb-4">
       {children}
