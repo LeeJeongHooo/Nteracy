b4ddcd0830143dc91a8ad2610efea98569b67492
leejeongho <jh9628771@gmail.com>
2024-11-02 13:42:29 +0900

    refact: 새로고침 시 두번 렌더링 수정

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -4,18 +4,20 @@ import "@assets/styles/main.css";
 
 __webpack_public_path__ = chrome.runtime.getURL("");
 
-let oldHref = "";
+let oldHref = window.location.href;
 
 window.onload = async () => {
-  console.log("window onLoad");
   if (window.location.search !== "" && window.location.search.includes("v=")) {
     await init();
   }
 
   const bodyList = document.querySelector("body");
-  let observer = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      if (oldHref !== document.location.href) {
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((_) => {
+      if (
+        oldHref !== document.location.href &&
+        document.location.search.includes("v=")
+      ) {
         oldHref = document.location.href;
         init();
       }
@@ -25,15 +27,8 @@ window.onload = async () => {
 };
 
 const init = async () => {
-  if (document.querySelector("#digital-literacy")) {
-    document.querySelector("#digital-literacy").remove();
-  }
-
   const appContainer = document.createElement("div");
   appContainer.id = "digital-literacy";
-  if (!appContainer) {
-    throw new Error("새로운 Dom이 생성되지 않았습니다.");
-  }
 
   await waitForElement("#secondary.style-scope.ytd-watch-flexy");
   if (document.querySelector("#digital-literacy")) {
@@ -51,13 +46,13 @@ const init = async () => {
   root.render(<App />);
 };
 
-export const waitForElement = (selector: string) => {
+const waitForElement = (selector: string) => {
   return new Promise((resolve) => {
     if (document.querySelector(selector)) {
       return resolve(document.querySelector(selector));
     }
 
-    const observer = new MutationObserver((mutations) => {
+    const observer = new MutationObserver((_) => {
       if (document.querySelector(selector)) {
         resolve(document.querySelector(selector));
         observer.disconnect();
