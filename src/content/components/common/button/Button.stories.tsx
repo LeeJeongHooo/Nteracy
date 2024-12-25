52c5fd6afc9b64334dbc3c1b69ae20552eedab9a
leejeongho <jh9628771@gmail.com>
2024-12-25 22:01:42 +0900

    feat: 버튼 스토리북 생성

---
+++ src/content/components/common/button/Button.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, StoryObj } from "@storybook/react";
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+  title: "Component/Button",
+  component: Button,
+  parameters: {
+    layout: "fullscreen",
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Primary: Story = {
+  args: {
+    children: "Primary",
+    disabled: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Primary.args,
+    disabled: true,
+  },
+};
