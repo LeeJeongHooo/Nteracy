b604d4c23a1e3988f70bd15407ba69fbba7789ed
leejeongho <jh9628771@gmail.com>
2025-01-23 21:24:41 +0900

    fix: ulti함수 분리로 인한 index.tsx 수정

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,29 +1,25 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import { waitForElement } from "@utils/waitForElement";
 import "@assets/styles/main.css";
+import App from "./App";
+import { setUpDomObserver } from "@utils/domObserver";
 
 __webpack_public_path__ = chrome.runtime.getURL("");
 
 let oldHref = window.location.href;
-
 window.onload = async () => {
   if (window.location.search !== "" && window.location.search.includes("v=")) {
     await init();
   }
-
-  const bodyList = document.querySelector("body");
-  const observer = new MutationObserver((mutations) => {
-    mutations.forEach((_) => {
-      if (
-        oldHref !== document.location.href &&
-        document.location.search.includes("v=")
-      ) {
-        oldHref = document.location.href;
-        init();
-      }
-    });
-  });
-  observer.observe(bodyList, { childList: true, subtree: true });
+  setUpDomObserver(() => {
+    if (
+      oldHref !== document.location.href &&
+      document.location.search.includes("v=")
+    ) {
+      oldHref = document.location.href;
+      init();
+    }
+  }, "body");
 };
 
 const init = async () => {
@@ -31,8 +27,10 @@ const init = async () => {
   appContainer.id = "digital-literacy";
 
   await waitForElement("#secondary.style-scope.ytd-watch-flexy");
-  if (document.querySelector("#digital-literacy")) {
-    document.querySelector("#digital-literacy").remove();
+
+  const existingAppContainer = document.querySelector("#digital-literacy");
+  if (existingAppContainer) {
+    existingAppContainer.remove();
   }
   const selectPosition = document.querySelector(
     "#secondary.style-scope.ytd-watch-flexy"
@@ -44,24 +42,4 @@ const init = async () => {
 
   const root = createRoot(appContainer);
   root.render(<App />);
-};
-
-const waitForElement = (selector: string) => {
-  return new Promise((resolve) => {
-    if (document.querySelector(selector)) {
-      return resolve(document.querySelector(selector));
-    }
-
-    const observer = new MutationObserver((_) => {
-      if (document.querySelector(selector)) {
-        resolve(document.querySelector(selector));
-        observer.disconnect();
-      }
-    });
-
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-    });
-  });
 };
