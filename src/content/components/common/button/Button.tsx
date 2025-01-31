f1279662796ff1ebe243a522825f4bf25025f7df
leejeongho <jh9628771@gmail.com>
2025-01-31 17:56:59 +0900

    fix: 버튼 컴포넌트 스타일 확장

--- src/content/components/common/button/Button.tsx
+++ src/content/components/common/button/Button.tsx
@@ -1,15 +1,19 @@
+import { cn } from "@utils/cn";
 import { ComponentPropsWithoutRef, forwardRef } from "react";
 
 export interface ButtonProps extends ComponentPropsWithoutRef<"button"> {}
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   (props, ref) => {
-    const { disabled, children, ...restProps } = props;
+    const { disabled, children, className, ...restProps } = props;
 
     return (
       <button
         ref={ref}
-        className="flex items-center justify-center text-base font-medium rounded px-3.5 py-2.5 transition-colors bg-primary300 text-white cursor-pointer hover:bg-grey600 disabled:pointer-events-none disabled:bg-grey400 disabled:text-grey500"
+        className={cn(
+          "flex items-center justify-center text-base font-medium rounded px-3.5 py-2.5 transition-colors bg-primary300 text-white cursor-pointer hover:bg-grey600 disabled:pointer-events-none disabled:bg-grey400 disabled:text-grey500",
+          className
+        )}
         disabled={disabled}
         {...restProps}
       >
