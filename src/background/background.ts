16f086417d597a51e16335711992208758e15d1b
leejeongho <jh9628771@gmail.com>
2025-02-20 21:10:44 +0900

    fix: 새로고침 시 HMR 동기화 문제 해결

--- src/background/background.ts
+++ src/background/background.ts
@@ -1,24 +1,25 @@
+import { AccountStatus, BackgroundRequest } from "./constant";
+
 if (module.hot) {
   require("@cooby/crx-load-script-webpack-plugin/lib/loadScript");
 }
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("BackGround접속");
+  console.log("Background Install");
 });
 
-enum AccountStatus {
-  SYNC = "SYNC",
-  ANY = "ANY",
+if (process.env.NODE_ENV === "development") {
+  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url?.startsWith("chrome://")) return undefined;
+
+    if (
+      changeInfo.status === "complete" &&
+      tab.url?.startsWith("https://www.youtube.com")
+    ) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabId },
+        files: ["content.js"], // DevServer의 최신 번들 경로
+      });
+    }
+  });
 }
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request === "oauth") {
-    chrome.identity.getProfileUserInfo(
-      { accountStatus: AccountStatus.ANY },
-      function (userInfo) {
-        sendResponse(userInfo);
-      }
-    );
-    return true;
-  }
-});
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,27 @@
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common.js");
+const path = require("path");
+const fs = require("fs");
+
+const updateManifest = () => {
+  const manifestPath = path.resolve(__dirname, "src/static/manifest.json");
+  const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf-8"));
+
+  manifest.content_scripts = [
+    { matches: ["https://www.youtube.com/*"], js: ["content.js"] },
+  ];
+
+  const distManifestPath = path.resolve(__dirname, "dist/manifest.json");
+  fs.writeFileSync(distManifestPath, JSON.stringify(manifest, null, 2));
+};
 
 module.exports = merge(common, {
   mode: "production",
+  plugins: [
+    {
+      apply: (compiler) => {
+        compiler.hooks.afterEmit.tap("UpdateManifestPlugin", updateManifest);
+      },
+    },
+  ],
 });
