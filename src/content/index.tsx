f15f3c4cda225ff93cbddb7e68c6d1cb838531a2
leejeongho <jh9628771@gmail.com>
2025-02-20 21:09:04 +0900

    refact: yt-navigate-finish 이벤트로 컴포넌트 init함수 실행

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -2,44 +2,44 @@ import { createRoot } from "react-dom/client";
 import { waitForElement } from "@utils/waitForElement";
 import "@assets/styles/main.css";
 import App from "./App";
-import { setUpDomObserver } from "@utils/domObserver";
 
 __webpack_public_path__ = chrome.runtime.getURL("");
 
-let oldHref = window.location.href;
-window.onload = async () => {
-  if (window.location.search !== "" && window.location.search.includes("v=")) {
-    await init();
-  }
-  setUpDomObserver(() => {
-    if (
-      oldHref !== document.location.href &&
-      document.location.search.includes("v=")
-    ) {
-      oldHref = document.location.href;
-      init();
-    }
-  }, "body");
-};
+const appId = "nteracy";
+const selector = "#secondary.style-scope.ytd-watch-flexy";
 
 const init = async () => {
   const appContainer = document.createElement("div");
-  appContainer.id = "digital-literacy";
+  appContainer.id = appId;
 
-  await waitForElement("#secondary.style-scope.ytd-watch-flexy");
+  const element = await waitForElement(selector);
 
-  const existingAppContainer = document.querySelector("#digital-literacy");
+  const existingAppContainer = document.getElementById(appId);
   if (existingAppContainer) {
     existingAppContainer.remove();
   }
-  const selectPosition = document.querySelector(
-    "#secondary.style-scope.ytd-watch-flexy"
-  );
 
-  if (selectPosition) {
-    selectPosition.insertAdjacentElement("afterbegin", appContainer);
+  if (element) {
+    element.insertAdjacentElement("afterbegin", appContainer);
   }
 
   const root = createRoot(appContainer);
+
   root.render(<App />);
+  isInitialize = false;
 };
+
+let isInitialize = false;
+
+if (location.pathname === "/watch" && !isInitialize) {
+  isInitialize = true;
+  init();
+}
+
+window.addEventListener("yt-navigate-finish", () => {
+  console.log("navigate-finish");
+  if (location.pathname === "/watch" && !isInitialize) {
+    isInitialize = true;
+    init();
+  }
+});
