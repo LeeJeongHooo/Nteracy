700c374407e6ed88beb3c9fd4f4f20cef8b766e8
leejeongho <jh9628771@gmail.com>
2025-03-23 06:06:47 +0900

    feat: Text 컴포넌트 생성

---
+++ src/content/components/common/txt/Text.tsx
@@ -0,0 +1,13 @@
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
+
+type TextProps<T extends ElementType> = {
+  as?: T;
+} & ComponentPropsWithoutRef<T>;
+
+export const Text = <T extends ElementType = "span">(props: TextProps<T>) => {
+  const { as, ...restProps } = props;
+
+  const Component = as || "span";
+
+  return <Component {...restProps} />;
+};
