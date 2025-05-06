3a70daf3ae0f174d221242e78c8c7ce76b545874
leejeongho <jh9628771@gmail.com>
2025-05-06 17:37:41 +0900

    fix: Card 컴포넌트 변경

--- src/content/components/card/Card.tsx
+++ src/content/components/card/Card.tsx
@@ -1,16 +1,27 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, ReactNode } from "react";
+import { Text } from "../common/txt/Text";
+import { cn } from "@utils/cn";
 
 interface CardProps {
-  title: String;
+  title: string;
+  children?: ReactNode;
+  className?: string | undefined;
 }
 
-export const Card = (props: PropsWithChildren<CardProps>) => {
-  const { children, title } = props;
+export const Card = (props: CardProps) => {
+  const { title, className, children } = props;
 
   return (
-    <div className="m-3 p-2 bg-white rounded overflow-hidden shadow-[0_3px_8px_rgb(0,0,0,0.2)] nt-left-border">
-      <h3 className="text-primary300 text-base font-medium">{title}</h3>
-      <div className="flex flex-col mt-2">{children}</div>
+    <div
+      className={cn(
+        "m-3 p-2 bg-white rounded overflow-hidden shadow-[0_3px_8px_rgb(0,0,0,0.2)] nt-left-border",
+        className
+      )}
+    >
+      <Text as="h3" className="text-primary300 text-base font-medium">
+        {title}
+      </Text>
+      {children && <div className="flex flex-col mt-2">{children}</div>}
     </div>
   );
 };
